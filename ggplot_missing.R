543b71855ba5f5c46ff77fb54796f3715525629b
sarance <Sarance@Sarances-MacBook-Pro-965.local>
2017-04-13 11:52:44 -0400

    ggplot_missing values

---
+++ ggplot_missing.R
@@ -0,0 +1,21 @@
+#plot missing data
+library(reshape2)
+library(ggplot2)
+library(magrittr)
+
+ggplot_missing <- function(x){
+  
+  x %>% 
+    is.na %>%
+    melt %>%
+    ggplot(data = .,
+           aes(x = Var2,
+               y = Var1)) +
+    geom_raster(aes(fill = value)) +
+    scale_fill_grey(name = "",
+                    labels = c("Present","Missing")) +
+    theme_minimal() + 
+    theme(axis.text.x  = element_text(angle=45, vjust=0.5)) + 
+    labs(x = "Variables in Dataset",
+         y = "Rows / observations")
+}
